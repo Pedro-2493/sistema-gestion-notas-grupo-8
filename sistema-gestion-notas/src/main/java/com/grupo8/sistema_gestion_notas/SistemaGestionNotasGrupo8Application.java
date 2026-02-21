965a906eba9e25454682f2905a1e369e3fdb9094
pedro-2493 <pedrozamora1993@gmail.com>
2026-02-21 11:55:37 -0500

    Primer commit proyecto Spring Boot sistema gestión notas

---
+++ sistema-gestion-notas/src/main/java/com/grupo8/sistema_gestion_notas/SistemaGestionNotasGrupo8Application.java
@@ -0,0 +1,13 @@
+package com.grupo8.sistema_gestion_notas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionNotasGrupo8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionNotasGrupo8Application.class, args);
+	}
+
+}
---
+++ sistema-gestion-notas/src/test/java/com/grupo8/sistema_gestion_notas/SistemaGestionNotasGrupo8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo8.sistema_gestion_notas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionNotasGrupo8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
